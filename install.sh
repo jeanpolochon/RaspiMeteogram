82d3709a5646e10e942fccafaafc20dc4771ce51
Jean-Paul Marcade <marcade.jp@gmail.com>
2019-04-15 22:50:26 +0100

    script for acquisition and update to the install script to save the configuration in a .conf file

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 mkdir -p ./log
-touch ./log/log\install.txt
+touch ./log/log_install.txt
 
 echo "Making sure that the OS is up to date"
-apt-get update -y >>./log/log\install.txt
-apt-get upgrade -y >>./log/log\install.txt
+apt-get update -y >>./log/log_install.txt
+apt-get upgrade -y >>./log/log_install.txt
 
 echo "Installing the required packages"
 echo "Installing apache web server"
-apt-get install apache2 -y >>./log/log\install.txt
+apt-get install apache2 -y >>./log/log_install.txt
 echo "Installing PHP"
-apt-get install php -y  >>./log/log\install.txt
+apt-get install php -y  >>./log/log_install.txt
 echo "Installing MySQL"
-apt-get install mysql-server python-mysqldb php-mysql -y  >>./log/log\install.txt
+apt-get install mysql-server python-mysqldb php-mysql -y  >>./log/log_install.txt
 
 echo "Restarting Apache webserver"
 service apache2 restart
@@ -31,11 +31,16 @@ if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
 MAINDB=${user//[^a-zA-Z0-9]/_}
 echo "Mysql user will be '$MAINDB' and database name will be '$MAINDB'"
 
-mysql --execute "CREATE DATABASE IF NOT EXISTS ${MAINDB};" >>./log/log\install.txt
-mysql --execute "CREATE USER IF NOT EXISTS ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';" >>./log/log\install.txt
-mysql --execute "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';" >>./log/log\install.txt
-mysql --execute "FLUSH PRIVILEGES;" >>./log/log\install.txt
+mysql --execute "CREATE DATABASE IF NOT EXISTS ${MAINDB};" >>./log/log_install.txt
+mysql --execute "CREATE USER IF NOT EXISTS ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';" >>./log/log_install.txt
+mysql --execute "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';" >>./log/log_install.txt
+mysql --execute "FLUSH PRIVILEGES;" >>./log/log_install.txt
 
-mysql --execute "CREATE TABLE IF NOT EXISTS ${MAINDB}.records(tdate DATE, ttime TIME, temperature DECIMAL(4,1), pressure DECIMAL(5,1), humidity DECIMAL(4,1));" >>./log/log\install.txt
+mysql --execute "CREATE TABLE IF NOT EXISTS ${MAINDB}.records(tdatetime DATETIME, temperature DECIMAL(4,1), pressure DECIMAL(5,1), humidity DECIMAL(4,1));" >>./log/log_install.txt
 
+echo [db-meteogram]>db.conf
+echo username=${MAINDB}>>db.conf
+echo password=${PASSWDDB}>>db.conf
+echo database=${MAINDB}>>db.conf
+echo table=records>>db.conf
 echo "Database is set up"
